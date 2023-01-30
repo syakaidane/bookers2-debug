ecfd299bd1dca6f3d1a5a2521101eac410f03db3
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-01-30 04:58:57 +0000

    commitk3

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'homes/top'
   root to: "homes#top"
   get "home/about"=>"homes#about"
-
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
 end
