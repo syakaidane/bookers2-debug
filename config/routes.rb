c85b18f7f1f116040a880e84d5bdcc21bf81cb8e
syakaidane <121926213+syakaidane@users.noreply.github.com>
2023-02-01 10:58:37 +0000

    commitk4

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,9 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
